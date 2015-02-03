11fe556967b084fa00d3c0742cdc6a72cb6939bc
Thaddeus R R Boyd <thad@daddyslick.com>
2015-02-03 13:07:22 -0700

    Styled tabs; still need to work on colors.

--- index.php
+++ index.php
@@ -11,17 +11,23 @@ require('includes/header.php');
 
 <div id="tabs">
   <nav><ul>
-    <li><a href="#websites">Websites</a></li>
-    <li><a href="#audiobooks">Audiobooks</a></li>
+    <li class="tab tab-websites"><a href="#websites">
+      <span class="fa fa-globe"></span><br/>
+      Websites
+    </a></li>
+    <li class="tab tab-audiobooks"><a href="#audiobooks">
+      <span class="fa fa-book"></span><br/>
+      Audiobooks
+    </a></li>
   </ul></nav>
   
-  <section id="websites">
+  <section id="websites" class="tab-content tab-websites">
     <h2>Websites</h2>
     
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem.</p>
   </section><!-- websites -->
   
-  <section id="audiobooks">
+  <section id="audiobooks" class="tab-content tab-audiobooks">
     <h2>Audiobooks</h2>
     
     <p>Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
