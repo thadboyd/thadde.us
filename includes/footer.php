d7c1a8973aba786f905d484e98da8de00fca1698
Thaddeus R R Boyd <thad@daddyslick.com>
2015-02-09 11:04:29 -0700

    Added Amazon Associates modal to footer.  Needs to be styled.

--- includes/footer.php
+++ includes/footer.php
@@ -10,8 +10,32 @@
 </div><!-- mainContent -->
 
 <footer>
-Original Content Copyright &copy; 2008-2015 Thaddeus Boyd<br/>
-Audiobooks, their descriptions and covers are copyright their respective rightsholders
+<p>Original Content Copyright &copy; 2008-2015 Thaddeus Boyd</p>
+
+<p>Additional copyright and trademark information</p>
+
+<p><a id="aaf-link" href="#amazon-associates-notification">Amazon Associates notification</a></p>
+
+<div id="copyrights-trademarks">
+</div><!-- copyrights-trademarks -->
+
+<div id="amazon-associates-notification" title="Amazon Associates notification">
+<p>Thaddeus Boyd is a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to amazon.com.</p>
+</div><!-- amazon-associates-notification -->
+
+<script>
+$(function() {
+  $("#amazon-associates-notification").dialog({
+    modal: true,
+    autoOpen: false
+  });
+  
+  $("#aaf-link").on("click", function() {
+    $("#amazon-associates-notification").dialog("open");
+  });
+});
+</script>
+
 </footer>
 
 </body>
